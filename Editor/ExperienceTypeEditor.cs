fbfab6710bb91baec6791378aa88f37f6772fc66
nrvllrgrs <nrvllrgrs@users.noreply.github.com>
2026-05-06 15:48:03 -0500

    Update ExperienceTypeEditor.cs

--- Editor/ExperienceTypeEditor.cs
+++ Editor/ExperienceTypeEditor.cs
@@ -1,192 +1,78 @@
-using System.Collections;
-using System.Linq;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
-using UnityEditorInternal;
-using ToolkitEngine.XP;
+using ToolkitEngine.Inventory;
 
-namespace ToolkitEditor.XP
+namespace ToolkitEngine.XP
 {
-	[CustomEditor(typeof(ExperienceType))]
-    public class ExperienceTypeEditor : BaseToolkitEditor
+	[CreateAssetMenu(menuName = "Toolkit/XP/Experience")]
+	public class ExperienceType : ScriptableObject
     {
 		#region Fields
 
-		protected ExperienceType m_experienceType;
+		[SerializeField]
+		private string m_id = Guid.NewGuid().ToString();
 
-		protected SerializedProperty m_id;
-		protected SerializedProperty m_name;
-		protected SerializedProperty m_description;
-		protected SerializedProperty m_levels;
-		private ReorderableList m_levelsList;
+		[SerializeField]
+		private string m_name;
 
-		private float m_maxValue = int.MinValue;
-		private float m_viewWidth = 1f;
+		[SerializeField, Multiline(3)]
+		private string m_description;
 
-		private const string MAX_WIDTH_NAME = "m_maxValue";
-		private const string REWARDS_NAME = "m_rewards";
+		[SerializeField]
+		private Sprite m_icon;
+
+		[SerializeField]
+		private List<Level> m_levels = new();
+
+		#endregion
+
+		#region Properties
+
+		public string id => m_id;
+		public new string name => m_name;
+		public string description => m_description;
+		public Sprite icon => m_icon;
+		public int maxLevel => m_levels.Count + 1;
+		public int maxValue => m_levels[^1].maxValue;
+		public IList<Level> levels => m_levels;
 
 		#endregion
 
 		#region Methods
 
-		private void OnEnable()
+		public bool TryGetRequiredValue(int level, out int value)
 		{
-			m_experienceType = target as ExperienceType;
-
-			m_id = serializedObject.FindProperty(nameof(m_id));
-			m_name = serializedObject.FindProperty(nameof(m_name));
-			m_description = serializedObject.FindProperty(nameof(m_description));
-			m_levels = serializedObject.FindProperty(nameof(m_levels));
-
-			UpdateMaxValue();
-		}
-
-		protected override void DrawProperties()
-		{
-			if (m_levelsList == null)
+			if (0 < level && level <= m_levels.Count)
 			{
-				m_levelsList = new ReorderableList((IList)m_experienceType.levels, typeof(Level), true, true, true, true);
-				m_levelsList.drawHeaderCallback += (Rect rect) =>
-				{
-					EditorGUI.LabelField(rect, "Levels");
-				};
-				m_levelsList.drawElementCallback += OnDrawElementCallback;
-				m_levelsList.onCanAddCallback += OnCanAddCallback;
-				m_levelsList.onAddDropdownCallback += OnAddDropdownCallback;
-				m_levelsList.onCanRemoveCallback += OnCanRemoveCallback;
-				m_levelsList.onRemoveCallback += OnRemoveCallback;
+				value = m_levels[level - 1].maxValue;
+				return true;
 			}
 
-			EditorGUI.BeginDisabledGroup(true);
-			EditorGUILayout.PropertyField(m_id, new GUIContent("ID"));
-			EditorGUI.EndDisabledGroup();
-
-			EditorGUILayout.PropertyField(m_name);
-			EditorGUILayout.PropertyField(m_description);
-
-			EditorGUI.BeginChangeCheck();
-			m_levelsList.DoLayoutList();
-
-			if (EditorGUI.EndChangeCheck())
-			{
-				UpdateMaxValue();
-			}
-
-			if (m_levelsList.index.Between(0, m_levelsList.count))
-			{
-				EditorGUILayout.PropertyField(
-					GetPropertyAt(m_levelsList.index, REWARDS_NAME),
-					new GUIContent($"Level {m_levelsList.index + 1} Rewards"));
-			}
-
-			EditorGUILayout.Separator();
-
-			EditorGUILayout.LabelField("Progress");
-
-			// Create space between layout properties and bar graph
-			Rect size = GUILayoutUtility.GetLastRect();
-			size.y += size.height + EditorGUIUtility.standardVerticalSpacing;
-			size.height = size.width;
-
-			if (size.height > 1)
-			{
-				m_viewWidth = size.height;
-			}
-
-			EditorGUILayout.BeginVertical(GUILayout.Height(m_viewWidth));
-			{
-				GUILayout.FlexibleSpace();
-
-				// Add 2-pixel space between each bar
-				float barHeight = (size.width - ((m_levels.arraySize - 1) * 2)) / m_levels.arraySize;
-				float barWidth = size.width;
-
-				float heightOffset = size.y + size.width;
-				for (int i = 0; i < m_levels.arraySize; ++i)
-				{
-					float value = GetPropertyAt(i, MAX_WIDTH_NAME).intValue / m_maxValue;
-
-					// Draw vertical bar
-					var pivot = new Vector2(size.x + (barHeight + 2) * i, heightOffset);
-
-					GUIUtility.RotateAroundPivot(-90f, pivot);
-					EditorGUI.DrawRect(new Rect(pivot.x, pivot.y, barWidth * value, barHeight), new Color(0.125f, 0.275f, 0.486f));
-					GUIUtility.RotateAroundPivot(90f, pivot);
-				}
-			}
-			EditorGUILayout.EndVertical();
-		}
-
-		private void UpdateMaxValue()
-		{
-			m_maxValue = int.MinValue;
-			for (int i = 0; i < m_levels.arraySize; ++i)
-			{
-				m_maxValue = Mathf.Max(
-					(m_levels.GetArrayElementAtIndex(i).boxedValue as Level).maxValue,
-					m_maxValue);
-			}
+			value = 0;
+			return false;
 		}
 
 		#endregion
+	}
 
-		#region ReorderableList Methods
+	[System.Serializable]
+	public class Level
+	{
+		#region Fields
 
-		private SerializedProperty GetPropertyAt(int index, string relativePropertyPath)
-		{
-			return m_levels.GetArrayElementAtIndex(index).FindPropertyRelative(relativePropertyPath);
-		}
+		[SerializeField]
+		private int m_maxValue;
 
-		private void OnDrawElementCallback(Rect rect, int index, bool isActive, bool isFocused)
-		{
-			var levelProp = GetPropertyAt(index, MAX_WIDTH_NAME);
-			var levelValue = levelProp.intValue;
-			int prevLevelValue = index > 0
-				? GetPropertyAt(index - 1, MAX_WIDTH_NAME).intValue
-				: 0;
+		[SerializeField]
+		private List<DropEntry> m_rewards = new();
 
-			Rect position = new Rect(rect);
-			position.width = EditorGUIUtility.labelWidth;
+		#endregion
 
-			// Draw row label
-			EditorGUI.LabelField(position, $"Level {index + 1}");
+		#region Properties
 
-			position.x += position.width;
-			position.width = (rect.width - position.width - EditorGUIUtility.standardVerticalSpacing) / 2f;
-
-			// Draw total value
-			levelProp.intValue = EditorGUI.IntField(position, levelValue);
-
-			EditorGUI.BeginDisabledGroup(true);
-			{
-				position.x += position.width + EditorGUIUtility.standardVerticalSpacing;
-
-				// Draw delta value
-				EditorGUI.IntField(position, levelValue - prevLevelValue);
-			}
-			EditorGUI.EndDisabledGroup();
-		}
-
-		private bool OnCanAddCallback(ReorderableList list) => true;
-
-		private void OnAddDropdownCallback(Rect buttonRect, ReorderableList list)
-		{
-			m_experienceType.levels.Add(new Level());
-			UpdateMaxValue();
-		}
-
-		private bool OnCanRemoveCallback(ReorderableList list)
-		{
-			return m_experienceType.levels != null && m_experienceType.levels.Count > 0;
-		}
-
-		private void OnRemoveCallback(ReorderableList list)
-		{
-			// Remove faction from list
-			m_experienceType.levels.RemoveAt(list.index);
-			UpdateMaxValue();
-		}
+		public int maxValue => m_maxValue;
+		public DropEntry[] rewards => m_rewards.ToArray();
 
 		#endregion
 	}
